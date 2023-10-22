0682add518631dac6eeee1d8bab1058d8d57e6b3
Animesh Singh <animeshsingrol@gmail.com>
2023-10-22 09:33:33 +0530

    signup with style alert done

--- signup.js
+++ signup.js
@@ -6,15 +6,32 @@ let login_btn = document.querySelector(".login_btn");
 login_btn.addEventListener("click", registerData);
 
 function registerData() {
-  if (email.value.length == 0) {
-    alert("please Enter Email");
-  } else if (password.value.length == 0) {
-    alert("please Enter Password");
-  } else if (user_name.value.length == 0) {
-    alert("please Enter User Name");
-  } else {
+  if (user_name.value.length == 0) {
+    showAlert("please Enter User Name");
+  } 
+  else if (email.value.length == 0) {
+    showAlert("please Enter Email");
+  } 
+  else if (password.value.length == 0) {
+    showAlert("please Enter Password");
+  } 
+  else {
     loadData(email.value, password.value, user_name.value);
   }
+}
+
+function showAlert(message) {
+  const alertDiv = document.createElement('div');
+  alertDiv.className = 'custom-alert'; // Apply the custom CSS class
+  alertDiv.textContent = message;
+
+  // Add the alert to the page (e.g., inside a specific container)
+  document.body.appendChild(alertDiv);
+
+  // Remove the alert after a certain time (optional)
+  setTimeout(() => {
+    document.body.removeChild(alertDiv);
+  }, 3000); // Remove after 3 seconds (adjust as needed)
 }
 
 async function loadData(email, password, username){
@@ -29,3 +46,10 @@ async function loadData(email, password, username){
   alert("Registration Successfull!");
   window.location.href = "login.html";
 }
+
+document.querySelector('.hamburger').addEventListener('click', function() {
+  var navbarLinks = document.querySelectorAll('.navbar a');
+  navbarLinks.forEach(function(link) {
+      link.style.display = (link.style.display === 'block') ? 'none' : 'block';
+  });
+});
